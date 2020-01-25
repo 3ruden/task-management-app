5fae8ba87c753bb393201de55ad5c564ead3206f
micchan <p1r2yy9knr@gmail.com>
2020-01-25 13:31:41 +0900

    Taskモデルを作成

---
+++ db/migrate/20200125042337_create_tasks.rb
@@ -0,0 +1,14 @@
+class CreateTasks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.text :content
+      t.datetime :deadline
+      t.integer :status
+      t.integer :priority
+
+      t.timestamps
+    end
+
+  end
+end
