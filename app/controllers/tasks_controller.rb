94bb2d719c8e73a85a8099b690043eced722af7b
micchan <p1r2yy9knr@gmail.com>
2020-01-26 14:05:03 +0900

    タスクのCRUD機能を実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: %i(show)
+  before_action :set_task, only: %i(show edit update destroy)
 
   def index
     @tasks = Task.all
@@ -15,9 +15,42 @@ class TasksController < ApplicationController
   def edit
   end
 
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to @task, notice: 'タスクを作成しました'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to @task, notice: 'タスクを編集しました'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_url, notice: 'タスクを削除しました'
+  end
+
   private
 
   def set_task
     @task = Task.find(params[:id])
   end
+
+  def task_params
+    params.require(:task).permit(
+      :title,
+      :content,
+      :deadline,
+      :status,
+      :priority,
+    )
+  end
+
 end
